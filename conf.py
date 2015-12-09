bb9bf0948be782e8d99c10c1ef6ce3eda5177f08
shelper <shelpermisc@gmail.com>
2015-12-09 17:12:08 -0500

    add conf.py

---
+++ conf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file specifies all the general system configurations
+
+TODO: define what is need to be configured here
+
+"""
+
+# pyoct.image
+vox_size = [x, y, z] # voxel size in each dimension, for <3d images, just use the former ones
+
+# pyoct.hardware
+## pyoct.hardware.scanner
+
+
--- pyoct/image/__init__.py
+++ pyoct/image/__init__.py
@@ -5,8 +5,15 @@ pyoct.image
 ~~~~~~~~~
 
 this module contains tools to manipulate general images, 8bit, 16bit, 24bit, etc.
-merge: merge2d, merge3d
-    merge different channels
+
+set_dimension:
+set_voxsize:
+
+display:
+    display image, default to adjust the image size based on the vox_size
+
+merge: merge2d: merge3d
+    merge different color/measurement channels
 
 project:
     min/max intensity projection, mean projection
